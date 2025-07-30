d52984dbdef52b64165d630d82a18ead789365f3
muhammadbilalaqeel <bilal_aqeel_intern@netixsol.com>
2025-07-30 19:28:08 +0500

    Home Page Done

--- Week 1/Day 3/task1/tailwind.config.js
+++ Week 1/Day 3/task1/tailwind.config.js
@@ -10,7 +10,8 @@ export default {
       backgroundImage:{
         'hero' : "url('../assets/hero.jpg')",
         'desert' : "url('../assets/hisma_desert_1.png')",
-        'explore' : "url('../assets/explore_bg.png')"
+        'explore' : "url('../assets/explore_bg.png')",
+        'destination' : "url('../assets/destination_bg.png')"
       }
      },
    },
