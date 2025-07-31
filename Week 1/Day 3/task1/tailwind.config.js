33ef31b1d44292078b7d611a1b2650d0e3d05cb5
muhammadbilalaqeel <bilal_aqeel_intern@netixsol.com>
2025-07-31 21:01:50 +0500

    Changing in tailwind.config file

--- Week 1/Day 3/task1/tailwind.config.js
+++ Week 1/Day 3/task1/tailwind.config.js
@@ -1,23 +1,23 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-  darkMode:'class',
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {
-      fontFamily:{
-        poppin:['"Poppins"', 'sans-serif'],
-        imperial : ['"Imperial Script"', 'cursive']
+  darkMode: "class",
+  content: ["./index.html", "./*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppin: ['"Poppins"', "sans-serif"],
+        imperial: ['"Imperial Script"', "cursive"],
       },
-      backgroundImage:{
-        'hero' : "url('../assets/hero.jpg')",
-        'desert' : "url('../assets/hisma_desert_1.png')",
-        'explore' : "url('../assets/explore_bg.png')",
-        'destination' : "url('../assets/destination_bg.png')",
-        'about':"url('../assets/about_bg.png')",
-        'a' : "url('../assets/a.png')",
-        'd' : "url('../assets/d.png')",
+      backgroundImage: {
+        hero: "url('../assets/hero.jpg')",
+        desert: "url('../assets/hisma_desert_1.png')",
+        explore: "url('../assets/explore_bg.png')",
+        destination: "url('../assets/destination_bg.png')",
+        about: "url('../assets/about_bg.png')",
+        a: "url('../assets/a.png')",
+        d: "url('../assets/d.png')",
       },
-     },
-   },
-   plugins: [],
- }
+    },
+  },
+  plugins: [],
+};
