612a004ff38097e2748f41fac1a7afb671540245
muhammadbilalaqeel <bilal_aqeel_intern@netixsol.com>
2025-07-31 10:24:14 +0500

    Start Scripting of Dark Mode

---
+++ Week 1/Day 3/task1/src/script.js
@@ -0,0 +1,6 @@
+
+// Dark/Light Mode Toggler
+
+let modeToggler = document.querySelector('.mode_toggler');
+let darkIcon = document.querySelector('.dark');
+let lightIcon = document.querySelector('.light');
